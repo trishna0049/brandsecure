d617b73cd8f63c71683c9007499eb74a08573333
trishna0049 <trishna0049@gmail.com>
2026-02-17 18:51:38 +0530

    Initial backend setup with auth

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get("/", (req, res) => {
+    res.send("API is running...");
+});
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("MongoDB Connected");
+
+    app.listen(process.env.PORT, () => {
+        console.log(`Server running on port ${process.env.PORT}`);
+    });
+})
+.catch(err => console.log("Mongo Error:", err));
