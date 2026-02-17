08f25a8daa8c31108ed55e61059970bbf9579cdb
trishna0049 <trishna0049@gmail.com>
2026-02-17 20:30:17 +0530

    compliance

--- server/server.js
+++ server/server.js
@@ -7,23 +7,22 @@ dotenv.config();
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Test Route
+// 🔥 AUTH ROUTE (IMPORTANT)
+app.use("/api/auth", require("./routes/authRoutes"));
+
+// TEST ROUTE
 app.get("/", (req, res) => {
     res.send("API is running...");
 });
 
-// MongoDB Connection
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
     console.log("MongoDB Connected");
-
     app.listen(process.env.PORT, () => {
         console.log(`Server running on port ${process.env.PORT}`);
     });
 })
-.catch(err => console.log("Mongo Error:", err));
-app.use("/api/compliance", require("./routes/complianceRoutes"));
+.catch(err => console.log(err));
